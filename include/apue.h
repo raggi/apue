4da34dd78cc2efe9899b8498167356b053caec2d
James Tucker <jftucker@gmail.com>
2012-05-30 12:12:25 -0700

    Turn on SuSv3 compliance on OSX >= 10.4

--- include/apue.h
+++ include/apue.h
@@ -8,6 +8,7 @@
 #define CMSG_LEN(x)	_CMSG_DATA_ALIGN(sizeof(struct cmsghdr)+(x))
 #elif !defined(BSD)
 #define _XOPEN_SOURCE	600	/* Single UNIX Specification, Version 3 */
+#define _DARWIN_C_SOURCE 1 /* OSX >= 10.4 - see compat(5) for more info */
 #endif
 
 #include <sys/types.h>		/* some systems still require this */
